41fb5eb5e4bd132f16587a6d603ae062b139360a
Chris Kaiser <chris.a.kaiser.7@gmail.com>
2024-01-27 01:21:11 -0500

    updated for prod

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `fill me in when we deploy`;
-export const perPage = 2;
+// export const endpoint = `https://gearfront-a6911dbdde1b.herokuapp.com/`
+export const prodEndpoint = `https://gearfront-a6911dbdde1b.herokuapp.com/`
+export const perPage = 8;
