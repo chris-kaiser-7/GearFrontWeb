de163aa7cecada7e2c4c75b0b98518b5bfcb2bad
Chris Kaiser <chris.a.kaiser.7@gmail.com>
2024-01-28 19:22:18 -0500

    test

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:3000/api/graphql`;
+export const endpoint = `https://localhost:3000/api/graphql`;
 // export const endpoint = `https://gearfront-a6911dbdde1b.herokuapp.com/`
-export const prodEndpoint = `http://gearfront-a6911dbdde1b.herokuapp.com/`
+// export const prodEndpoint = `https://gearfront-a6911dbdde1b.herokuapp.com/`
+export const prodEndpoint =  `https://localhost:3000/api/graphql`
 export const perPage = 8;
